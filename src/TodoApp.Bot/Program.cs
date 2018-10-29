3836c1062fd5e2bd80a857953e75c57430bf6bef
Miguel Veloso <mvelosop@gmail.com>
2018-10-29 18:50:59 +0000

    Update indentation to spaces.

--- src/TodoApp.Bot/Program.cs
+++ src/TodoApp.Bot/Program.cs
@@ -7,32 +7,32 @@ using Microsoft.Extensions.Logging;
 
 namespace TodoApp.Bot
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			BuildWebHost(args).Run();
-		}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
 
-		public static IWebHost BuildWebHost(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-				.ConfigureLogging((hostingContext, logging) =>
-				{
-					// Add Azure Logging
-					logging.AddAzureWebAppDiagnostics();
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    // Add Azure Logging
+                    logging.AddAzureWebAppDiagnostics();
 
-					// Logging Options.
-					// There are other logging options available:
-					// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1
-					// logging.AddDebug();
-					// logging.AddConsole();
-				})
+                    // Logging Options.
+                    // There are other logging options available:
+                    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1
+                    // logging.AddDebug();
+                    // logging.AddConsole();
+                })
 
-				// Logging Options.
-				// Consider using Application Insights for your logging and metrics needs.
-				// https://azure.microsoft.com/en-us/services/application-insights/
-				// .UseApplicationInsights()
-				.UseStartup<Startup>()
-				.Build();
-	}
+                // Logging Options.
+                // Consider using Application Insights for your logging and metrics needs.
+                // https://azure.microsoft.com/en-us/services/application-insights/
+                // .UseApplicationInsights()
+                .UseStartup<Startup>()
+                .Build();
+    }
 }
